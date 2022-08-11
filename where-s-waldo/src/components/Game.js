d8ba1a8ffd6493a07791ca4794f4a7092a374e95
williamphk <65807958+williamphk@users.noreply.github.com>
2022-08-12 00:04:45 +0800

    v2

--- where-s-waldo/src/components/Game.js
+++ where-s-waldo/src/components/Game.js
@@ -1,6 +1,6 @@
 import "./Game.css";
 import image from "./level-1.jpg";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Game() {
   const Waldo = { minX: 522, maxX: 550, minY: 358, maxY: 441 };
@@ -11,6 +11,11 @@ function Game() {
   const [imageX, setImageX] = useState(0);
   const [imageY, setImageY] = useState(0);
   const [toggleMenu, setToggleMenu] = useState(false);
+  const [selection, setSelection] = useState(null);
+  const [clickCharacter, setClickCharacter] = useState(null);
+  const [correctWaldo, setCorrectWaldo] = useState(false);
+  const [correctOdlaw, setCorrectOdlaw] = useState(false);
+  const [correctWizard, setCorrectWizard] = useState(false);
 
   const menuStyle = {
     position: "absolute",
@@ -18,43 +23,80 @@ function Game() {
     left: `${mouseX}px`,
   };
 
-  const handleClick = (e) => {
+  const handleImageClick = (e) => {
     e.preventDefault();
     setToggleMenu(!toggleMenu);
     setImageX(e.pageX - e.target.offsetLeft);
     setImageY(e.pageY - e.target.offsetTop);
     setMouseX(e.clientX + 1);
     setMouseY(e.clientY + 1);
-    console.log(imageX, imageY);
+  };
+
+  const handleMenuClick = (e) => {
+    setSelection(e.target.textContent);
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
     if (imageX > Waldo.minX && imageX < Waldo.maxX && imageY > Waldo.minY && imageY < Waldo.maxY) {
-      console.log("Waldo");
+      setClickCharacter("Waldo");
     } else if (
       imageX > Odlaw.minX &&
       imageX < Odlaw.maxX &&
       imageY > Odlaw.minY &&
       imageY < Odlaw.maxY
     ) {
-      console.log("Odlaw");
+      setClickCharacter("Odlaw");
     } else if (
       imageX > Wizard.minX &&
       imageX < Wizard.maxX &&
       imageY > Wizard.minY &&
       imageY < Wizard.maxY
     ) {
-      console.log("Wizard");
-    } else {
-      console.log("Wrong");
+      setClickCharacter("Wizard");
     }
-  };
+  }, [
+    imageX,
+    Waldo.minX,
+    Waldo.maxX,
+    Waldo.minY,
+    Waldo.maxY,
+    imageY,
+    Odlaw.minX,
+    Odlaw.maxX,
+    Odlaw.minY,
+    Odlaw.maxY,
+    Wizard.minX,
+    Wizard.maxX,
+    Wizard.minY,
+    Wizard.maxY,
+  ]);
+
+  useEffect(() => {
+    if (selection === "Waldo" && clickCharacter === "Waldo") {
+      setCorrectWaldo(true);
+    } else if (selection === "Odlaw" && clickCharacter === "Odlaw") {
+      setCorrectOdlaw(true);
+    } else if (selection === "Wizard" && clickCharacter === "Wizard") {
+      setCorrectWizard(true);
+    } else setSelection(null);
+  }, [selection, clickCharacter]);
+
+  useEffect(() => {
+    console.log(`selection ${selection}`, `clickCharacter ${clickCharacter}`, correctWaldo);
+  }, [selection, clickCharacter, correctWaldo]);
 
   return (
     <div className="Game">
+      <div>{correctWaldo ? "Waldo ✔" : null}</div>
+      <div>{correctOdlaw ? "Odlaw ✔" : null}</div>
+      <div>{correctWizard ? "Wizard ✔" : null}</div>
       <ul className={toggleMenu ? "menu" : "menu off"} style={menuStyle}>
-        <li>Waldo</li>
-        <li>Odlaw</li>
-        <li>Wizard</li>
+        <li onClick={handleMenuClick}>Waldo</li>
+        <li onClick={handleMenuClick}>Odlaw</li>
+        <li onClick={handleMenuClick}>Wizard</li>
       </ul>
-      <img src={image} alt="Map" onClick={handleClick} />
+      <img src={image} alt="Map" onClick={handleImageClick} />
     </div>
   );
 }
